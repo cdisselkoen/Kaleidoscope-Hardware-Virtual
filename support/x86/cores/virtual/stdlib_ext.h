050143f67e1a7ca337e85154661f404bcfebed8e
noseglasses <shinynoseglasses@gmail.com>
2017-11-27 13:05:22 +0100

    Added Stream.cpp and fixed use of _cplusplus
    The implementation of the Stream class is missing. I added only
    those missing parts that are needed to copile an executable based
    on Kaleidoscope-Hardware-Virtual. Without this, the symbol would
    be reported as undefined during linking.
    
    The _cplusplus in stdlib_ext.h was misspelled it must actualy read "__cplusplus"
    to work.

--- support/x86/cores/virtual/stdlib_ext.h
+++ support/x86/cores/virtual/stdlib_ext.h
@@ -1,7 +1,7 @@
 // These functions are defined in the avr stdlib.h, but not the standard one
 // see descriptions in tools/avr/avr/include/stdlib.h
 
-#ifdef _cplusplus
+#ifdef __cplusplus
 extern "C" {
 #endif
 
@@ -13,6 +13,6 @@ char *ultoa(unsigned long val, char* s, int radix);
 char *dtostre(double val, char* s, unsigned char prec, unsigned char flags);
 char *dtostrf(double val, signed char width, unsigned char prec, char* s);
 
-#ifdef _cplusplus
+#ifdef __cplusplus
 }
 #endif
